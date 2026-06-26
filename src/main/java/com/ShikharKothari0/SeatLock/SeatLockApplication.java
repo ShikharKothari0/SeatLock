21731deebe72d87bc359bc08f031f3a037909def
Shikhar Kothari <kothari.sahara@gmail.com>
2026-06-26 23:01:35 +0530

    Initial commit

---
+++ src/main/java/com/ShikharKothari0/SeatLock/SeatLockApplication.java
@@ -0,0 +1,12 @@
+package com.ShikharKothari0.SeatLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatLockApplication.class, args);
+	}
+}
